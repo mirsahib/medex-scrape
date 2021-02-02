5d46d821759d4e38a74537be70092fd80ea92672
mirsahib <mirsahib24@gmail.com>
2021-02-02 20:28:25 +0600

    :hammer: Add test home route

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require("express");
 
+//server configuration
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  try {
+    return res.json({ msg: "home" });
+  } catch (error) {
+    return res.status(500).json({ error: error.message });
+  }
+});
+
+app.use("/medlist", require("./routes/med-list.route"));
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
